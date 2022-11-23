0817f9c0ebdb45dee60797d419585784f060f9b5
foofoo3 <3139640799@qq.com>
2022-11-23 18:18:45 +0800

    商家缓存

--- src/main/java/com/hmdp/controller/ShopTypeController.java
+++ src/main/java/com/hmdp/controller/ShopTypeController.java
@@ -1,9 +1,14 @@
 package com.hmdp.controller;
 
-
+import cn.hutool.core.util.StrUtil;
+import cn.hutool.json.JSONUtil;
+import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import com.hmdp.dto.Result;
 import com.hmdp.entity.ShopType;
+import com.hmdp.mapper.ShopTypeMapper;
 import com.hmdp.service.IShopTypeService;
+import com.hmdp.utils.RedisConstants;
+import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -21,14 +26,37 @@ import java.util.List;
  */
 @RestController
 @RequestMapping("/shop-type")
-public class ShopTypeController {
+public class ShopTypeController extends ServiceImpl<ShopTypeMapper, ShopType> implements IShopTypeService {
+//    @Resource
+//    private IShopTypeService typeService;
     @Resource
-    private IShopTypeService typeService;
+    private StringRedisTemplate stringRedisTemplate;
 
     @GetMapping("list")
     public Result queryTypeList() {
-        List<ShopType> typeList = typeService
-                .query().orderByAsc("sort").list();
-        return Result.ok(typeList);
+//        List<ShopType> typeList = typeService
+//                .query().orderByAsc("sort").list();
+//        return Result.ok(typeList);
+
+        //1.从redis中查询店铺类型缓存
+        String shopType = stringRedisTemplate.opsForValue().get(RedisConstants.CACHE_SHOPTYPE_KEY);
+        //2.判断是否为空
+        if (StrUtil.isNotBlank(shopType)) {
+            //3.存在，直接返回
+            List<ShopType> shopTypes = JSONUtil.toList(shopType, ShopType.class);
+            return Result.ok(shopTypes);
+        }
+        //4.不存在，从数据库中查询写入redis
+        List<ShopType> shopTypes = query().orderByAsc("sort").list();
+        //5.不存在，返回错误
+        if (shopTypes == null) {
+            return Result.fail("分类不存在");
+        }
+        //6.存在，写入redis
+        stringRedisTemplate.opsForValue().set(RedisConstants.CACHE_SHOPTYPE_KEY,JSONUtil.toJsonStr(shopTypes));
+        //7.返回
+        return Result.ok(shopTypes);
+
     }
+
 }
