3f093d4661f7c8eda22fd15712a4e132de427ca3
foofoo3 <3139640799@qq.com>
2022-11-29 16:25:59 +0800

    Update VoucherOrderServiceImpl.java

--- src/main/java/com/hmdp/service/impl/VoucherOrderServiceImpl.java
+++ src/main/java/com/hmdp/service/impl/VoucherOrderServiceImpl.java
@@ -8,8 +8,10 @@ import com.hmdp.service.ISeckillVoucherService;
 import com.hmdp.service.IVoucherOrderService;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import com.hmdp.utils.RedisIdWorker;
+import com.hmdp.utils.SimpleRedisLock;
 import com.hmdp.utils.UserHolder;
 import org.springframework.aop.framework.AopContext;
+import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -33,6 +35,9 @@ public class VoucherOrderServiceImpl extends ServiceImpl<VoucherOrderMapper, Vou
     @Resource
     private RedisIdWorker redisIdWorker;
 
+    @Resource
+    private StringRedisTemplate stringRedisTemplate;
+
     @Override
     public Result seckillVoucher(Long voucherId) {
         // 1.查询优惠券
@@ -54,11 +59,26 @@ public class VoucherOrderServiceImpl extends ServiceImpl<VoucherOrderMapper, Vou
         }
 
         Long id = UserHolder.getUser().getId();
-        synchronized(id.toString().intern()) {
-//            获取代理对象（事务）
+
+//        创建锁对象
+        SimpleRedisLock lock = new SimpleRedisLock("order:" + id, stringRedisTemplate);
+//        获取锁
+        boolean isLock = lock.tryLock(1200);
+
+        if (!isLock){
+            return Result.fail("不允许重复下单");
+        }
+
+        try {
+            //            获取代理对象（事务）
             IVoucherOrderService proxy = (IVoucherOrderService) AopContext.currentProxy();
             return proxy.createVoucherOrder(voucherId);
+        }finally {
+//            释放锁
+            lock.unlock();
         }
+
+
     }
 
     @Override
